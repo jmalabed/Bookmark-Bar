0fec0da7f5d7927b15fcca4e8277cd9331cd32e3
jmalabed <jmalabed27@gmail.com>
2021-08-25 11:29:14 -0700

    server packages referenced and middleware listed

--- server.js
+++ server.js
@@ -4,10 +4,42 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+mongoose.connect('mongodb://localhost:27017/resources',{useNewUrlParser: true});
+
+const db = mongoose.connection
+db.once('open',()=>{
+  console.log('connected to mongo');
+});
+
 
 // =============================
-//         LISTEN ROUTE
+//         MIDDLEWARE
 // =============================
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
+// =============================
+//         ROUTING
+// =============================
+app.get('/index',(req,res)=>{
+  res.send
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// =============================
+//           LISTEN
+
 app.listen(port, ()=>{
   console.log("listening on port", port);
 })
