7f3aa3e7c77dc4334dc7c7a286af4958644fdcb2
piraiojeda <piraiojeda@gmail.com>
2021-08-25 20:26:09 -0700

    bug fixed

---
+++ seeds/topicData.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      name: "JavaScript",
+      img: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+    },
+    {
+      name: "HTML",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/HTML5_logo_black.svg/2048px-HTML5_logo_black.svg.png"
+    },
+    {
+      name: "CSS",
+      img: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png"
+    }
+]
