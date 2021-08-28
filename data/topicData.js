d5012df262a28694c42828bed41f7b22da35230f
jmalabed <jmalabed27@gmail.com>
2021-08-27 23:40:51 -0700

    added topics and logo for data

--- data/topicData.js
+++ data/topicData.js
@@ -10,5 +10,21 @@ module.exports = [
     {
       name: "CSS",
       img: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png"
+    },
+    {
+      name: "Node.js",
+      img: "https://cdn.freebiesupply.com/logos/thumbs/2x/nodejs-1-logo.png"
+    },
+    {
+      name: "Mongoose.js",
+      img: "https://cms-assets.tutsplus.com/uploads/users/34/posts/29527/preview_image/mongoose.jpg"
+    },
+    {
+      name: "Python",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2048px-Python-logo-notext.svg.png"
+    },
+    {
+      name: "MongoDB",
+      img: "https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png"
     }
 ]
