f60280ab75c7c4ba63c84b6d747f6453dd6d70cd
jmalabed <jmalabed27@gmail.com>
2021-09-01 23:57:51 -0700

    added seeder script

---
+++ seedscript.js
@@ -0,0 +1,32 @@
+//required libs
+const MongoClient = require('mongodb').MongoClient;
+const topicData = require('./data/topicData.js');
+const resourceData = require('./data/resourceData.js')
+const blogData = require('./data/blogData.js')
+
+async function seedDB(){
+  const uri = "mongodb+srv://jarJared:MAoz35TUV418dcZ0@cluster0.xuy8n.mongodb.net/bar?retryWrites=true&w=majority"
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+  })
+
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+
+    const blogCollection = client.db("bar").collection("blogs")
+    const resourceCollection = client.db("bar").collection("resources")
+    const topicCollection = client.db("bar").collection("topics")
+    blogCollection.insertMany(blogData);
+    resourceCollection.insertMany(resourceData);
+    topicCollection.insertMany(topicData);
+
+    console.log('database seeded :)');
+
+    client.close();
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+seedDB();
